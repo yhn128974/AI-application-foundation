a61dc666fdf3004593396ce93f2082c84ffbb170
longyou <yuhaonan@outlook.com>
2026-05-02 08:50:20 +0800

    update

---
+++ python/base/hello.py
@@ -0,0 +1,66 @@
+# s1 = 72
+# s2 = 85
+# s3=(s2-s1)/s1
+# print('小明的成绩提升了%.2f%%.'%(s3*100))
+# print('小明的成绩提高了{0:.2f}%'.format(s3*100))
+# print('小明的成绩提高了{s3*100.2f}%')
+
+#字面量的写法
+# print(True)
+# print(False)
+# print("hello python")
+# print(None)#空类型
+
+# print(True+1)
+# print(False-1)
+
+
+# #变量
+# num=100.1;
+# num=2000;
+# print(num)
+# num='ok'
+# print(num)
+# base=20.1;
+# mon=50;
+# print("播放总量为",base+mon*3)
+
+# #数据类型
+# print(type('string'))
+# print(type(None))
+# num=100;
+# print(type(num))
+# #类型判定 
+# print(isinstance('string',str)) 
+# print(isinstance('string',int)) 
+
+# #字符串
+# s3="""
+# hello
+# world!
+# """
+# print(s3)
+# s1=' it\'s am apple ' #\ 转译操作
+# s2="hello 的意思是'你好' "
+# print(s1)
+# print(s2)
+
+# s4=' \t欢迎你\n的到来' #\n 换行,\t缩进
+# print(s4)
+
+# #字符串拼接
+# print(s2+s4)
+# name='hello'
+# age=20
+# address='chongqing'
+# print("我是%s,年龄%s,住在%s" %(name,age,address))
+# #字符串前面加f 拼接在{}里面
+# print(f"你好{name},年龄{age}，地址{address}")
+
+
+# #转化为字符串
+# num4=100
+# print("hello"+str(num4) )
+
+
+
