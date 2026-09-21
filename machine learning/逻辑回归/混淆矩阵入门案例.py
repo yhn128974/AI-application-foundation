226cefe49cf576b982a5b89781ee0013c92f7226
longyou <yuhaonan@outlook.com>
2026-09-21 15:37:26 +0800

    chore: update project files

--- machine learning/逻辑回归/混淆矩阵入门案例.py
+++ machine learning/逻辑回归/混淆矩阵入门案例.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+
 
 # 准备数据:
 # 有10个样本，6个恶性肿瘤，4个良性肿瘤，我们假设恶性为正样本，良性为负样本
@@ -30,6 +32,30 @@ df_cm_a=pd.DataFrame(cm_a,index=['真实悪性','真实良性'],columns=['预测
 df_cm_b=pd.DataFrame(cm_b,index=['真实悪性','真实良性'],columns=['预测悪性','预测良性'])
 print(f"混淆矩阵A:\n{df_cm_a}")
 print(f"混淆矩阵B:\n{df_cm_b}")
+print(f"\n")
+
+# 准确率：预测正确的样本数/总样本数
+print(f"模型a的准确率为：{accuracy_score(test_y,predict_a)}")
+print(f"模型b的准确率为：{accuracy_score(test_y,predict_b)}")
+print(f"\n")
+
+# 精确率：预测正确的正样本数/预测为正样本的总数
+print(f"模型a的精确率为：{precision_score(test_y,predict_a,pos_label='恶性')}")
+print(f"模型b的精确率为：{precision_score(test_y,predict_b,pos_label='恶性')}")
+print(f"\n")
+
+# 召回率：预测正确的正样本数/实际为正样本的总数
+print(f"模型a的召回率为：{recall_score(test_y,predict_a,pos_label='恶性')}")
+print(f"模型b的召回率为：{recall_score(test_y,predict_b,pos_label='恶性')}")
+print(f"\n")
+
+# F1值：精确率和召回率的调和平均值
+print(f"模型a的F1值为：{f1_score(test_y,predict_a,pos_label='恶性')}")
+print(f"模型b的F1值为：{f1_score(test_y,predict_b,pos_label='恶性')}")
+print(f"\n")
+
+# 
+
 
 # 数据预处理
 
